5ca2719452dfe434d28045d8db0e2f29a4223ba8
katvengo <katvengo@gmail.com>
2024-05-18 06:29:42 -0700

    added authenticator

--- recipe-app/src/app/page.tsx
+++ recipe-app/src/app/page.tsx
@@ -1,19 +1,30 @@
+'use client';
+
 import React from 'react'
 import RecipeForm from '../components/RecipeForm/RecipeForm'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 export default function App() {
   return (
     <>
     <div className="container">
-     <h1>Recipe App</h1>
-    </div>
+      <h1>PrepCado - The new way to create and save recipes!</h1>
+      </div>
+    <Authenticator>
+      {({signOut, user}) => (
+    <main>
     <div className="container">
     <h2>Create a Recipe Below</h2>
     </div>
     <div className="container">
     <RecipeForm></RecipeForm>
-    </div>
+    <button onClick={signOut}>Sign out</button>
 
+    </div>  
+    </main>    
+  )}
+</Authenticator>
      </>
   );
 }

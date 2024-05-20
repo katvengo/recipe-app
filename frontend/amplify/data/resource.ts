b62163610c074d1006cfba5f67acbe87bf7648c5
katvengo <katvengo@gmail.com>
2024-05-20 16:57:12 -0700

    Updated folder structure

--- frontend/amplify/data/resource.ts
+++ frontend/amplify/data/resource.ts
@@ -1,13 +1,15 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
+/*== STEP 1 ===============================================================
+The section below creates a Todo database table with a "content" field. Try
+adding a new "isDone" field as a boolean. The authorization rule below
+specifies that any unauthenticated user can "create", "read", "update", 
+and "delete" any "Todo" records.
+=========================================================================*/
 const schema = a.schema({
-  recipe: a
+  Todo: a
     .model({
-      name: a.string(),
-      description: a.string(),
-      categories: a.string(),
-      ingredients: a.string(),
-      directions: a.string()
+      content: a.string(),
     })
     .authorization((allow) => [allow.guest()]),
 });

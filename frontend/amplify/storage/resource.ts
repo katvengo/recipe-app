527437cb3436e4a805d50dbdccef6a2309f40bce
katvengo <katvengo@gmail.com>
2024-06-01 21:06:30 -0700

    Updated permissions for image upload

--- frontend/amplify/storage/resource.ts
+++ frontend/amplify/storage/resource.ts
@@ -3,9 +3,13 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
     name: 'recipeImageStorage',
     access: (allow) => ({
-        'recipe-photos/{identityId}/*': [
-          allow.guest.to(['read']),
-          allow.entity('identity').to(['read', 'write', 'delete'])
-        ],
-      })
+      'recipe-photos/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'profile-photos/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+    })
 })
